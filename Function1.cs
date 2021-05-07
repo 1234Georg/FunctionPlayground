6ac46c3cefa266ed6c6b14e9cb5343a3ae36530a
Georg Mayer <Georg.Mayer@gmail.com>
2021-05-07 18:28:16 +0200

    Urls ergänzt

--- Function1.cs
+++ Function1.cs
@@ -13,6 +13,15 @@ using System.Linq;
 
 namespace FunctionPlayground
 {
+    /*
+Details:
+  Azure Resource Group: https://ms.portal.azure.com/#resource/subscriptions/d7feceb8-a7a2-43a6-884d-317219b9ea5b/resourceGroups/FunctionPlayground-dev-rg/overview
+  Azure Storage: https://ms.portal.azure.com/#resource/subscriptions/d7feceb8-a7a2-43a6-884d-317219b9ea5b/resourceGroups/FunctionPlayground-dev-rg/providers/Microsoft.Storage/storageAccounts/functionplayground2
+  Azure Function App: http://FunctionPlayground-dev-as.azurewebsites.net
+  Azure DevOps Project: FunctionPlayground (https://clt-57830401-33b2-4c94-b6b2-0ccd75eea17d.visualstudio.com/FunctionPlayground)
+  Azure DevOps build pipeline: https://clt-57830401-33b2-4c94-b6b2-0ccd75eea17d.visualstudio.com/051c7269-fe8d-4a84-b6a8-053695b87718/_build/definition?definitionId=1
+  Azure DevOps release pipeline: https://clt-57830401-33b2-4c94-b6b2-0ccd75eea17d.visualstudio.com/051c7269-fe8d-4a84-b6a8-053695b87718/_release?definitionId=1
+     */
     public static class Function1
     {
         [FunctionName("Function1")]
